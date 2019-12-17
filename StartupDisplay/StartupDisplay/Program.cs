3f5e2550d60f8c3641afdcb85605c35bf62a7386
NoahGiles <billywerber@gmail.com>
2019-12-17 15:07:00 -0600

    Initial File Upload

---
+++ StartupDisplay/StartupDisplay/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = null;
+            Console.WriteLine("Welcome to Tic Tac Toe! Would you like to play Tic Tac Toe?");
+
+        }
+    }
+}
