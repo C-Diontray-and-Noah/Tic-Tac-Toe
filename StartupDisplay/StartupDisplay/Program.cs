a6c6836f52a0b79b1b91351a71cd4a87a10d8791
NoahGiles <billywerber@gmail.com>
2019-12-18 13:18:19 -0600

    Things Being Done

--- StartupDisplay/StartupDisplay/Program.cs
+++ StartupDisplay/StartupDisplay/Program.cs
@@ -11,8 +11,18 @@ namespace StartupDisplay
         static void Main(string[] args)
         {
             string userInput = null;
+            bool quit = false;
             Console.WriteLine("Welcome to Tic Tac Toe! Would you like to play Tic Tac Toe?");
+            userInput = Console.ReadLine();
+            while(quit == false)
+            if (userInput.ToLower() == "yes")
+            {
 
+            }
+            else if (userInput.ToLower() == "no")
+            {
+                    quit = true;
+            }
         }
     }
 }
